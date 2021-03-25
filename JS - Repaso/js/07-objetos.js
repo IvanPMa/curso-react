e16f42648de0387a3119622432a5ffd6616dbd25
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-25 15:45:52 -0600

    Objetos

---
+++ JS - Repaso/js/07-objetos.js
@@ -0,0 +1,13 @@
+// Objetos
+
+// object literal
+const persona = {
+    nombre: 'Juan',
+    profesion: 'Desarrollador web',
+    edad: 500
+}
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona.profesion);
+console.log(persona.edad);
+console.log(persona['edad']);
