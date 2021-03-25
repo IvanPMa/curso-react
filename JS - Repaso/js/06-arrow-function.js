6e7278fa7b56e4b7fd05a5d0fa78240bd0713148
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-25 15:36:49 -0600

    Arrow Functions

---
+++ JS - Repaso/js/06-arrow-function.js
@@ -0,0 +1,14 @@
+// Arrow functions
+
+let viajando = destino => `Viajando a la ciudad de: ${destino}`;
+
+
+
+// let viaje = viajando('Paris');
+
+let viaje;
+viaje = viajando('Paris');
+viaje = viajando('Londres');
+viaje = viajando('Barcelona');
+
+console.log(viaje);
