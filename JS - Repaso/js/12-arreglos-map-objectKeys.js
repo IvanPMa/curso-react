6e82efd655dd9da360ab2392d1a33c613296fb08
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-28 15:38:33 -0600

    Map and object keys

--- JS - Repaso/js/12-arreglos-map-objectKeys.js
+++ JS - Repaso/js/12-arreglos-map-objectKeys.js
@@ -2,6 +2,26 @@
 
 const carrito = ['Producto 1','Producto 2','Producto 3']
 console.log(carrito);
-
+/* 
 const appContenedor =document.querySelector('#app');
-appContenedor.innerHTML = carrito;
+let html ='';
+carrito.forEach(producto =>{
+    html += ` <li>${producto}</li>`
+});
+
+appContenedor.innerHTML = html;
+ */
+//Consulta y devuelve un nuevo arreglo
+console.log(carrito.map(producto =>{
+    return  `El producto es ${producto}`
+}));
+
+
+
+//Object keys
+const persona = {
+    nombre: 'Juan',
+    profesion : 'Desarrollador web',
+    edad: 500
+}
+console.log(Object.keys(persona));
