fbdeaf56a9d9144e8b83085fab8c8914338a05b7
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-28 22:30:43 -0600

    Promises,ajax,show in the html

--- JS - Repaso/js/16-promises-ajax.js
+++ JS - Repaso/js/16-promises-ajax.js
@@ -26,9 +26,26 @@ const descargarUsuarios = cantidad => new Promise((resolve,reject)=>{
 
 descargarUsuarios(20)
     .then(
-        miembros => console.log(miembros),
+        // miembros => console.log(miembros),
+        miembros => imprimirHTML(miembros),
         error => console.error(
             new Error('Hubo un error' + error)
         )
-    )
+    );
     
+function imprimirHTML(usuarios){
+    let html = '';
+    usuarios.forEach(usuario => {
+        html += `
+            <li>
+            Nombre: ${usuario.name.first} ${usuario.name.last}
+            País: ${usuario.nat}
+            Imagen:
+                <img src="${usuario.picture.medium}">
+            </li>
+        `;
+    });
+
+    const contenedorApp = document.querySelector('#app');
+    contenedorApp.innerHTML = html;
+} 
