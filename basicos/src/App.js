1ebd126d3c63e054b1908aef89b6851b730ec5e8
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-30 16:53:22 -0600

    Events on React

--- basicos/src/App.js
+++ basicos/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment,useState} from 'react';
 import Header from'./components/Header'
 import Footer from'./components/Footer'
+import Producto from'./components/Producto'
 
 function App() {
 
@@ -20,6 +21,13 @@ function App() {
         <Header 
           titulo = 'Tienda virtual'
         />
+        <h1>Lista de Productos</h1>
+        {productos.map(producto =>(
+          <Producto 
+            key = {producto.id}
+            producto = { producto}
+          />
+        ))}
         <Footer
           fecha = {fecha}
         />
