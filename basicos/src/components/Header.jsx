eddd88611b8f47f9618c1ec9125341c01f8ba5bb
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-30 12:00:34 -0600

    Primer componente

---
+++ basicos/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Creacion del componente usando function declaration
+function Header(){
+    // Antes del return podemos escribir codigo JS
+
+    // En el return es lo que se va a ver en pantalla
+    return(
+        <h1>Desde el header</h1>
+    );
+}
+
+export default Header;
