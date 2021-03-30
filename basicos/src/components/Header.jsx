ecc964fb6e589c76961c8ec475276b3af0007fce
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-30 12:12:12 -0600

    Basicos de JSX

--- basicos/src/components/Header.jsx
+++ basicos/src/components/Header.jsx
@@ -3,11 +3,20 @@ import React from 'react';
 // Creacion del componente usando function declaration
 function Header(){
     // Antes del return podemos escribir codigo JS
+    // const edad = 22;
+    // let mensaje;
+    // if(edad >= 18){
+    //     mensaje = 'Eres mayor de edad';
+    // } else{
+    //     mensaje= 'Eres mejor de edad';
+    // }
+
 
     // En el return es lo que se va a ver en pantalla
     return(
-        <h1>Desde el header</h1>
-    );
+        // <h1>Desde el header {edad}, {mensaje}</h1>
+        <h1 id='encabezado' className= 'encabezado'>Tienda virtual</h1>
+        );
 }
 
 export default Header;
